769dcbca80ad4ac7d9ad2d7014684a7caf354c19
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-01 01:32:31 +0200

    load spc

--- src/main.zig
+++ src/main.zig
@@ -3,17 +3,27 @@ const std = @import("std");
 const Smp = struct {
     const ramSize = 0x10000;
 
-    accumulator: u8,
-    xRegister: u8,
-    yRegister: u8,
-    status: u8,
-    stackPointer: u8,
-    programCounter: u16,
-    yaRegister: u16,
+    accumulator: u8 = 0,
+    xRegister: u8 = 0,
+    yRegister: u8 = 0,
+    status: u8 = 0,
+    stackPointer: u8 = 0,
+    programCounter: u16 = 0,
+    yaRegister: u16 = 0,
 
     ram: [ramSize]u8,
+
+    pub fn load(path: []const u8) !Smp {
+        const file = try std.fs.cwd().openFile(path, .{});
+        defer file.close();
+
+        return Smp{ .ram = undefined };
+    }
 };
 
 pub fn main() !void {
-    std.log.info("hi", .{});
+    const veryCool = "spc/ChronoTrigger/304 Corridors of Time.spc";
+    const smp = try Smp.load(veryCool);
+
+    _ = smp;
 }
