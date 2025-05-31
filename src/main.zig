8c0122beee52e695c5f3cfb5f2130feca0343ea7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-31 23:11:44 +0200

    sketch smp fields

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
 
+const Smp = struct {
+    const ramSize = 0x10000;
+
+    accumulator: u8,
+    xRegister: u8,
+    yRegister: u8,
+    status: u8,
+    stackPointer: u8,
+    programCounter: u16,
+    yaRegister: u16,
+
+    ram: [ramSize]u8,
+};
+
 pub fn main() !void {
     std.log.info("hi", .{});
 }
