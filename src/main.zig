ffb404ee661bb18b571e18dc527bfef14c87189e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-01 02:29:08 +0200

    simplify indexing by loading whole header

--- src/main.zig
+++ src/main.zig
@@ -25,34 +25,30 @@ const Smp = struct {
         defer file.close();
         const reader = file.reader();
 
-        var header: [0x23]u8 = undefined;
+        var header: [0x100]u8 = undefined;
         _ = try reader.read(&header);
         if (!std.mem.eql(u8, header[0..0x23], "SNES-SPC700 Sound File Data v0.30" ++ .{ 0x1a, 0x1a })) {
             return LoadError.NotAnSpcFile;
         }
 
-        const byte23 = try reader.readByte();
-        if (byte23 == 26) {
-            std.log.info("-- ID666 info inbound", .{});
-        } else if (byte23 == 27) {
-            std.log.info("-- No ID666 info inbound", .{});
-        } else {
+        switch (header[0x23]) {
+            26 => std.log.info("-- ID666 info inbound", .{}),
+            27 => std.log.info("-- No ID666 info inbound", .{}),
+            else => return LoadError.NotAnSpcFile,
+        }
+
+        if (header[0x24] != 30) {
             return LoadError.NotAnSpcFile;
         }
 
-        const byte25 = try reader.readByte();
-        if (byte25 != 30) {
-            return LoadError.NotAnSpcFile;
-        }
-
-        const pcl: u16 = try reader.readByte();
-        const pch: u16 = try reader.readByte();
+        const pcl: u16 = header[0x25];
+        const pch: u16 = header[0x26];
         const pc = pcl + 256 * pch;
-        const a = try reader.readByte();
-        const x = try reader.readByte();
-        const y = try reader.readByte();
-        const status = try reader.readByte();
-        const sp: u16 = try reader.readByte();
+        const a = header[0x27];
+        const x = header[0x28];
+        const y = header[0x29];
+        const status = header[0x2a];
+        const sp: u16 = header[0x2b];
 
         std.log.info("Registers:", .{});
         std.log.info("pc = {x:04}", .{pc});
@@ -61,11 +57,6 @@ const Smp = struct {
         std.log.info("y = {x:02}", .{y});
         std.log.info("p = {b:08}", .{status});
         std.log.info("sp = {x:02}", .{sp});
-
-        {
-            var unused: [2]u8 = undefined;
-            _ = try reader.read(&unused);
-        }
 
         return Smp{ .ram = undefined };
     }
